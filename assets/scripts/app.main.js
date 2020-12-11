6bbaef15c0d7448d6e3ab6dfbde8a3c85d5045d9
Sardor <asrorjon5538@gmail.com>
2020-12-11 10:07:14 +0500

    done))

--- assets/scripts/app.main.js
+++ assets/scripts/app.main.js
@@ -2,7 +2,9 @@ $(document).ready(function () {
     $('.burger-button').click(function (e) {
         e.preventDefault();
         $('.mobile-menu').addClass('active');
-        console.log('document work!!!')
+        console.log('document work!!!');
+        $('.overlay').addClass('active');
+        $('body').css({'overflow' : 'hidden', 'height' : '100vh'});
     });
     $('.close-btn').click(function (e) {
         e.preventDefault();
@@ -12,14 +14,14 @@ $(document).ready(function () {
         $('.overlay').removeClass('active')
 
     });
-    $('.burger-button').click(function () {
-        $('.overlay').addClass('active')
-        $('body').css('overflow', 'hidden')
-    });
+ 
     $('.overlay').click(function () {
         $('.mobile-menu').removeClass('active');
         $('.overlay').removeClass('active')
-        $('body').css('overflow', 'unset')
+        $('body').css({
+            'overflow': 'unset',
+            'height': 'initial'
+        })
 
     })
 })
