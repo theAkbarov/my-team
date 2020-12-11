772708232eb25c06ce09dcfab135742f789699b8
Sardor <asrorjon5538@gmail.com>
2020-12-11 10:00:04 +0500

    burger added

--- assets/scripts/app.main.js
+++ assets/scripts/app.main.js
@@ -1,12 +1,22 @@
-$(document).ready(function(){
-    $('.burger-button').click(function(e){
+$(document).ready(function () {
+    $('.burger-button').click(function (e) {
         e.preventDefault();
         $('.mobile-menu').addClass('active');
-        console.log('document work!!!')        
+        console.log('document work!!!')
     });
-    $('.close-btn').click(function(e){
+    $('.close-btn').click(function (e) {
         e.preventDefault();
-        $('.mobile-menu').removeClass('active')
-    });
+        $('.mobile-menu').removeClass('active');
+        $('.overlay').removeClass('active')
 
+    });
+    $('.burger-button').click(function () {
+        $('.overlay').addClass('active')
+        $('body').css('overflow', 'hidden')
+    });
+    $('.overlay').click(function () {
+        $('.mobile-menu').removeClass('active');
+        $('.overlay').removeClass('active')
+
+    })
 })
